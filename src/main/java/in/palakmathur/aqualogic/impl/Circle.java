4201c141c50279e36dbbd1191492810c6dfb0273
ZSI473 <palak.mathur@capitalone.com>
2015-01-01 23:12:44 -0600

    Using MessageSource To Get Text From Property Files

--- src/main/java/in/palakmathur/aqualogic/impl/Circle.java
+++ src/main/java/in/palakmathur/aqualogic/impl/Circle.java
@@ -1,6 +1,8 @@
 package in.palakmathur.aqualogic.impl;
 
 import in.palakmathur.aqualogic.Shape;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 
 import javax.annotation.PostConstruct;
@@ -11,6 +13,17 @@ import javax.annotation.Resource;
 public class Circle implements Shape {
 
     private Point center;
+
+    @Autowired
+    private MessageSource messageSource;
+
+    public MessageSource getMessageSource() {
+        return messageSource;
+    }
+
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     public Point getCenter() {
         return center;
@@ -23,7 +36,9 @@ public class Circle implements Shape {
 
     @Override
     public void draw() {
-        System.out.println("Circle drawn with Center of circle at = " + center);
+        System.out.println(this.messageSource.getMessage("drawing.circle",new
+                Object[] {center},"Default Greeting",null));
+        //System.out.println();
     }
 
     @PostConstruct
--- src/main/java/in/palakmathur/aqualogic/impl/DrawingApp.java
+++ src/main/java/in/palakmathur/aqualogic/impl/DrawingApp.java
@@ -3,21 +3,27 @@ package in.palakmathur.aqualogic.impl;
 
 import in.palakmathur.aqualogic.Shape;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
+//import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Locale;
 
 public class DrawingApp {
 
     public static void main(String[] args) {
 
-        AbstractApplicationContext applicationContext = new
+        //AbstractApplicationContext applicationContext = new
+        ApplicationContext applicationContext = new
                 ClassPathXmlApplicationContext("spring-config.xml"); //for this to work spring-config should be in the Classpath and src/main/resources folder is the one
-        applicationContext.registerShutdownHook();
+        //applicationContext.registerShutdownHook();
 
 
         Shape shape;
         shape = (Shape) applicationContext.getBean("circle");
         shape.draw();
+
+       // System.out.println(applicationContext.getMessage("greeting",null,
+           //     "Default Greeting", null ));
     }
 
 }
