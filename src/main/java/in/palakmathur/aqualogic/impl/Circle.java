f41bf66cb827d2f7225fdd7c8d3c87f530814432
ZSI473 <palak.mathur@capitalone.com>
2015-01-01 21:09:37 -0600

    JSR 250 annotations

--- src/main/java/in/palakmathur/aqualogic/impl/Circle.java
+++ src/main/java/in/palakmathur/aqualogic/impl/Circle.java
@@ -5,6 +5,10 @@ import in.palakmathur.aqualogic.Shape;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
 public class Circle implements Shape {
 
     private Point center;
@@ -13,14 +17,23 @@ public class Circle implements Shape {
         return center;
     }
 
-    @Autowired
-    @Qualifier("circleRelated")
+    @Resource
     public void setCenter(Point center) {
         this.center = center;
     }
 
     @Override
     public void draw() {
-        System.out.println("Center of circle is = " + center);
+        System.out.println("Circle drawn with Center of circle at = " + center);
+    }
+
+    @PostConstruct
+    public void initializeCircle(){
+        System.out.println("Initializing Circle");
+    }
+
+    @PreDestroy
+    public void destroyCircle(){
+        System.out.println("Destroying Circle");
     }
 }
--- src/main/java/in/palakmathur/aqualogic/impl/DrawingApp.java
+++ src/main/java/in/palakmathur/aqualogic/impl/DrawingApp.java
@@ -3,23 +3,16 @@ package in.palakmathur.aqualogic.impl;
 
 import in.palakmathur.aqualogic.Shape;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
 
-    /**
-     *
-     * @param args
-     */
     public static void main(String[] args) {
-        //Triangle triangle = new Triangle();
-        //BeanFactory factory =
-        // new XmlBeanFactory(new FileSystemResource("src/main/resources/spring-config.xml"));
 
-        //AbstractApplicationContext applicationContext = new
-        ApplicationContext applicationContext = new
+        AbstractApplicationContext applicationContext = new
                 ClassPathXmlApplicationContext("spring-config.xml"); //for this to work spring-config should be in the Classpath and src/main/resources folder is the one
-        // applicationContext.registerShutdownHook();
+        applicationContext.registerShutdownHook();
 
         Shape shape;
         shape = (Shape) applicationContext.getBean("circle");
